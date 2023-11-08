7e88e2de908d014b482dee448040730b006937d5
patpaw111 <mfaqih3562@gmail.com>
2023-11-08 13:15:59 +0700

    first commit

---
+++ adminpanel/index.php
@@ -0,0 +1,15 @@
+<?php
+    require "session.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h2>Halo Admin</h2>
+</body>
+</html>
---
+++ adminpanel/login.php
@@ -0,0 +1,54 @@
+<?php
+    require "../koneksi.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+</head>
+
+<style>
+    .main{
+        height: 100vh
+    }
+
+    .login-box{
+        width: 500px;
+        height: 300px;
+        box-sizing: border-box;
+        border-radius: 10px;
+    }
+</style>
+
+<body>
+    <div class="main d-flex flex-column justify-content-center align-items-center">
+        <div class="login-box p-5 shadow">
+            <form action="" method="post">
+                <div>
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" name="username" id="username">
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" name="password" id="password">
+                </div>
+                <div>
+                    <button class="btn btn-success form-control mt-3" type="submit" name="loginbtn">Login</button>
+                </div>
+            </form>
+        </div>
+
+        <div>
+            <?php
+                if(isset($_POST['loginbtn'])){
+                    echo "Disubmit";
+                }
+            ?>
+        </div>
+    </div>
+</body>
+</html>
