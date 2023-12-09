f117423d0360638d4ee31c0b1fb69c4c5fea9917
patpaw111 <mfaqih3562@gmail.com>
2023-12-10 05:54:27 +0700

    selesai membuat admin pandel

--- adminpanel/index.php
+++ adminpanel/index.php
@@ -1,5 +1,12 @@
 <?php
-require "session.php"
+    require "session.php";
+    require "../koneksi.php";
+
+    $queryKategori = mysqli_query($con, "SELECT * FROM  kategori");
+    $jumlahKategori = mysqli_num_rows($queryKategori);
+
+    $queryMenu = mysqli_query($con, "SELECT * FROM  menu");
+    $jumlahMenu = mysqli_num_rows($queryMenu);
 ?>
 
 <!DOCTYPE html>
@@ -10,20 +17,78 @@ require "session.php"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../fontawesome/css/fontawesome.min.css">
 </head>
+
+<style>
+    .kotak {
+        border: solid;
+    }
+
+    .summary-kategori {
+        background-color: #dc3545;
+        border-radius: 10px;
+    }
+
+    .summary-produk {
+        background-color: #0a516b;
+        border-radius: 10px;
+    }
+
+    .no-decoration {
+        text-decoration: none;
+    }
+</style>
 
 <body>
     <?php require "navbar.php"; ?>
     <div class="container mt-5">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item active" aria-current="page">Home</li>
+                <li class="breadcrumb-item active" aria-current="page">
+                    <i class="fas fa-home"></i> home
+                </li>
             </ol>
         </nav>
         <h2>Halo <?php echo $_SESSION['username'] ?></h2>
+
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-12 mb-3">
+                    <div class="summary-kategori p-3">
+                        <div class="row">
+                            <div class="col-6">
+                                <i class="fas fa-align-justify fa-7x text-black-50"></i>
+                            </div>
+                            <div class="col-6 text-white">
+                                <h3 class="fs-2">kategori</h3>
+                                <p class="fs-4"><?php echo $jumlahKategori; ?> Kategori</p>
+                                <a href="kategori.php" class="text-white no-decoration">lihat detail</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 mb-3">
+                    <div class="summary-produk p-3">
+                        <div class="row">
+                            <div class="col-6">
+                                <i class="fas fa-utensils fa-7x text-black-50"></i>
+                            </div>
+                            <div class="col-6 text-white">
+                                <h3 class="fs-2">Menu</h3>
+                                <p class="fs-4"><?php echo $jumlahMenu; ?> Menu</p>
+                                <a href="produk.php" class="text-white no-decoration">lihat detail</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="..//fontawesome/js/all.min.js"></script>
 </body>
 
 </html>
