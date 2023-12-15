895629d67a2226c6d00a084eb78788d854553610
patpaw111 <mfaqih3562@gmail.com>
2023-12-15 16:24:04 +0700

    menambahkan fitu tambah kategori

---
+++ adminpanel/kategori-detail.php
@@ -0,0 +1,99 @@
+<?php
+require "session.php";
+require "../koneksi.php";
+
+$id = $_GET['p'];
+
+$query = mysqli_query($con, "SELECT  * FROM kategori WHERE id='$id'");
+$data = mysqli_fetch_array($query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Kategori</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+    <?php require "navbar.php" ?>
+
+    <div class="container mt-5">
+        <h2>Detail Kategori</h2>
+
+        <div class="col-12 col-md-6">
+            <form action="" method="post">
+                <label for="kategori">Kategori</label>
+                <input type="text" name="kategori" id="kategori" class="form-control" value="<?php echo $data['nama']; ?>">
+
+                <div class="mt-5">
+                    <button type="submit" class="btn btn-primary" name="editBtn">Edit</button>
+                    <button type="submit" class="btn btn-danger" name="deleteBtn">Hapus</button>
+                </div>
+            </form>
+
+            <?php
+            if (isset($_POST['editBtn'])) {
+                $kategori = htmlspecialchars($_POST['kategori']);
+
+                if ($data['nama'] == $kategori) {
+                    ?>
+                        <meta http-equiv="refresh" content="0; url=kategori.php" />
+                    <?php
+                } else {
+                    $query = mysqli_query($con, "SELECT * FROM kategori WHERE nama='$kategori'");
+                    $jumlahData = mysqli_num_rows($query);
+
+                    if ($jumlahData > 0) {
+                        ?>
+                            <div class="alert alert-warning  mt-3" role="alert">
+                                Kategori Sudah ada
+                            </div>
+                        <?php
+                    }
+                    else{
+                        $querySimpan = mysqli_query($con, "UPDATE kategori SET nama='$kategori' WHERE id='$id'");
+
+                        if($querySimpan){
+                            ?>
+                            <div class="alert alert-warning  mt-3" role="alert">
+                                Kategori Berhasil Terupdate
+                            </div>
+    
+                            <meta http-equiv="refresh" content="0; url=kategori.php" /> 
+                            <?php
+                        }
+                        else{
+                            echo mysqli_error($con);
+                        }
+                    }
+                }
+            }
+
+            if(isset($_POST['deleteBtn'])){
+                $queryDelete = mysqli_query($con, "DELETE FROM kategori WHERE id='$id'");
+
+                if($queryDelete){
+                    ?>
+                        <div class="alert alert-warning  mt-3" role="alert">
+                             Kategori Berhasil Di Hapus
+                        </div>
+
+                        <meta http-equiv="refresh" content="0; url=kategori.php" />
+                    <?php
+                }
+                else{
+                    echo mysqli_error($con);
+                }
+            }
+            ?>
+        </div>
+    </div>
+
+    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- adminpanel/navbar.php
+++ adminpanel/navbar.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
     <div class="container">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler offcanvas-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse offcanvas-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item me-3">
                     <a class="nav-link" href="../adminpanel">Home</a>
@@ -12,7 +12,7 @@
                     <a class="nav-link me-3" href="kategori.php">Kategori</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link me-3" href="produk.php" >Produk</a>
+                    <a class="nav-link me-3" href="produk.php">Menu</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link me-3" href="logout.php">Logout</a>
@@ -21,3 +21,70 @@
         </div>
     </div>
 </nav>
+
+<style>
+    @media (max-width: 992px) {
+        body.no-scroll {
+            overflow: hidden;
+        }
+
+        .navbar-collapse {
+            position: fixed;
+            top: 0;
+            left: -50%; /* Setengah layar */
+            width: 50%; /* Setengah layar */
+            height: 100%;
+            background: #343a40;
+            transition: left 0.3s ease;
+            z-index: 1000;
+            padding-top: 56px; /* Sesuaikan dengan tinggi navbar */
+        }
+
+        .navbar-nav {
+            margin-top: 5px; /* Jarak 5px pada tulisan */
+        }
+
+        .navbar-nav .nav-link {
+            margin-left: 5px; /* Tambahkan margin kiri 5px pada tombol navigasi */
+            transition: background-color 0.3s ease; /* Efek transisi warna latar belakang saat hover */
+        }
+
+        .navbar-nav .nav-link:hover {
+            background-color: #dc3545; /* Warna merah yang sama saat hover */
+            color: #fff; /* Warna teks saat hover */
+        }
+
+        .navbar-collapse.show {
+            left: 0;
+        }
+
+        .close-btn {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            cursor: pointer;
+        }
+    }
+</style>
+
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        var navbarToggle = document.querySelector('.offcanvas-toggler');
+        var navbarCollapse = document.querySelector('.offcanvas-collapse');
+        var body = document.querySelector('body');
+
+        navbarToggle.addEventListener('click', function () {
+            navbarCollapse.classList.toggle('show');
+            body.classList.toggle('no-scroll');
+        });
+
+        // Menutup navbar saat mengklik di luar navbar
+        document.addEventListener('click', function (e) {
+            if (!navbarCollapse.contains(e.target) && !navbarToggle.contains(e.target)) {
+                navbarCollapse.classList.remove('show');
+                body.classList.remove('no-scroll');
+            }
+        });
+    });
+</script>
